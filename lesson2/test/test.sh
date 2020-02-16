e4bf780287684cb11adcb771d25a392639b571df
DeZMeNeR <dimasavchukua4@gmail.com>
2020-02-16 14:35:43 +0000

    lesson2

---
+++ lesson2/test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Something ..."
+whoami
+pwd
+
+echo "its my home director: $HOME"
+
+index=5
+name="Mike"
+echo "$name has index = $index"
+var1=$(( 10+ 20*4 ))
+var2=$(( $var1 / 2 ))
+echo $var1
+echo $var2
+
+
+if pwd
+then
+echo "works"
+if
+
+
+
+
